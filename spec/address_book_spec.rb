4211cfd1971e7fa8eeb6b36bbac1a4605515558c
Michael Yosowitz <michaelyosowitz@gmail.com>
2017-05-03 09:09:50 -0700

    assignment-6-addressbloc

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -78,4 +78,31 @@ RSpec.describe AddressBook do
       check_entry(entry_five, "Sussie", "555-555-2036", "sussie@blocmail.com")
     end
   end
+
+  describe "#import_from_csv" do
+    it "imports the correct number of entries" do
+      book.import_from_csv("entries_2.csv")
+      book_size = book.entries.size
+      expect(book_size).to eq 3
+    end
+
+    it "imports the 1st entry" do
+      book.import_from_csv("entries_2.csv")
+      entry_one = book.entries[0]
+      check_entry(entry_one, "Jane","777-77-7777","jane@blocmail.com")
+    end
+
+    it "imports the 2nd entry" do
+      book.import_from_csv("entries_2.csv")
+      entry_two = book.entries[1]
+      check_entry(entry_two, "Nasty","555-555-2222","nasty@blocmail.com")
+    end
+
+    it "imports the 3rd entry" do
+      book.import_from_csv("entries_2.csv")
+      check_entry(entry_three, "Woody","555-555-2312","woody@blocmail.com")
+    end
+  end
+
+
 end
